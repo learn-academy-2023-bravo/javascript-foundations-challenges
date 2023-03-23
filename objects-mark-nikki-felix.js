fb2099d005f5f48434f8566ffb66da1fa4b353a9
MarkNou <mark.nou95@gmail.com>
2023-03-23 10:25:12 -0700

    switching driver to nikki

--- objects-mark-nikki-felix.js
+++ objects-mark-nikki-felix.js
@@ -1,55 +1,107 @@
-// Consider this variable:
+// // Consider this variable:
 
-const person = {
-  firstName: "Arthur",
-  lastName: "Dent",
-  homePlanet: "Earth",
-  whereFrom: function () {
-    return `${this.firstName} ${this.lastName} is form planet ${this.homePlanet}`
-  }
-}
-// Write the code that accesses the first name of the person object.
-console.log(person.firstName)
-//output:Arthur
+// const person = {
+//   firstName: "Arthur",
+//   lastName: "Dent",
+//   homePlanet: "Earth",
+//   whereFrom: function () {
+//     return `${person.firstName} ${person.lastName} is form planet ${person.homePlanet}`
+//   }
+// }
+// // Write the code that accesses the first name of the person object.
+// console.log(person.firstName)
+// //output:Arthur
 
-// Write the code that accesses the last name of the person object.
-console.log(person.lastName)
-//output:Dent
+// // Write the code that accesses the last name of the person object.
+// console.log(person.lastName)
+// //output:Dent
 
-// Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
-console.log(person.homePlanet)
+// // Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+// console.log(person.homePlanet)
 
-// Update the person object with a method that logs "Arthur Dent is from planet Earth".
-console.log(person.whereFrom())
-//output:Arthur Dent is form planet Earth
-// Consider this variable:
+// // Update the person object with a method that logs "Arthur Dent is from planet Earth".
+// console.log(person.whereFrom())
+
+// const {firstName, lastName, homePlanet, whereFrom} = person
+
+// console.log(whereFrom())
+// //output:Arthur Dent is form planet Earth
+// // Consider this variable:
 
 // const product = {
 //   name: "chair",
 //   price: 24.99
 // }
-// Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
-// Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
-// Consider this variable:
+
+// // Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
+
+// //pseudocode
+// // create function called describePoint
+
+// const describeProduct = () => {
+//   return `The product is a ${product.name}. It cost ${product.price} ` 
+// }
+// console.log(describeProduct()) // outcome: The product is a chair. It cost 24.99 
+
+// // Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
+
+// const totalWithTax = () => {
+//   withTax = product.price * 1.07 
+//     return `$${withTax.toFixed(2)}`
+// }
+
+// console.log(totalWithTax())
+// // Consider this variable:
 
 // const lunch = {
 //   name: "PB and Banana",
 //   type: "sandwich",
 //   ingredients: ["bread", "peanut butter", "banana"]
 // }
-// Write the code that accesses the ingredients property.
-// Write the code that access the third ingredient of the lunch object.
-// Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
-// Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
-// Consider this variable:
+// // Write the code that accesses the ingredients property.
 
-// const animals = [
-//   { name: "Waffles", type: "dog", age: 7 },
-//   { name: "Fluffy", type: "cat", age: 14 },
-//   { name: "Spelunky", type: "dog", age: 4 },
-//   { name: "Hank", type: "cat", age: 11 }
-// ]
+// const ingredientsCode = () => {
+//   return lunch.ingredients
+// }
+// console.log(ingredientsCode())
+
+// // Write the code that access the third ingredient of the lunch object.
+// const ingredientCode = () => {
+//   return lunch.ingredients[2]
+// }
+// console.log(ingredientCode())
+
+// // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+// const pbNj = () => {
+//   return `The ingredients for a ${lunch.name} ${lunch.type} are ${lunch.ingredients[0]}, ${lunch.ingredients[1]}, and ${lunch.ingredients[2]}`
+// }
+
+// console.log(pbNj())
+// // Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+// // Consider this variable:
+
+const animals = [
+  { name: "Waffles", type: "dog", age: 7 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 }
+]
 // Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+
+// const type = animals.filter((value) => value.type === "cat")
+const type = animals.filter((value) => value.type === "cat")
+console.log(type)
+
+const onlyCats = () => {
+  let cats = animals.filter((value) => value.type === "cat")
+  return cats
+}
+
+
+
+
+
 // Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
 // Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
 // Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
